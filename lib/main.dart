d3caeb87263dce3cb0600a264a35473dbe12b9ad
AndragApps@gmail.com <contact@chiragsitapara.co.nz>
2025-05-05 16:50:58 +1200

    Final Commit done

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Text("Flutter App"),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey,
+          foregroundColor: Colors.white,
+        ),
+        backgroundColor: Colors.blueGrey[100],
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+              'https://blog.solguruz.com/wp-content/uploads/2023/11/Flutter-for-Hybrid-Apps-Why-Flutter-is-the-Best-Platform-to-Make-Hybrid-Apps.png',
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
